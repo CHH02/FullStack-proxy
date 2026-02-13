d021bf934caaf2571be32a6435c48a4519ae79ff
CHH02 <clayhop901@gmail.com>
2026-02-12 19:38:02 -0600

    Adding Cors

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
+app.use(cors({
+    origin: 'https://chh02.github.io/FullStack-countries/',
+    optionsSuccessStatus: 200 
+  }));
 
 app.get('/api/weather', async (req, res) => {
   try {
